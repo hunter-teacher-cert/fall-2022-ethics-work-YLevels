40482b56bffd5bf4e17deedd925ebac44cb41020
Yeidy Levels <103545784+YLevels@users.noreply.github.com>
2022-09-15 00:08:09 +0000

    success!

---
+++ py/binsearch.py
@@ -0,0 +1,26 @@
+# binsearch.py
+# Yeidy Levels
+# CSCI 77800 Fall 2022
+# collaborators: just me
+# consulted: ThinkPython; w3schools
+
+def binSearch(list, target):
+  low = 0
+  high = len(list) -1
+  middle = 0
+
+  while low <= high:
+    middle = (low + high) // 2
+
+    if (list[middle] == target):
+      return middle
+    elif ((list[middle]) > target):
+      high = middle - 1
+    elif ((list[middle]) < target):
+      low = middle + 1
+
+  return -1
+
+list = [5, 10, 15, 20, 40, 80, 100]
+print(binSearch(list, 40))
+  
